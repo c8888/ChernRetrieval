7791a54917b3a3a143c734a5c44671960e1af853
c8888 <pedal72@gmail.com>
2016-11-20 20:30:35 +0100

    Initial commit

---
+++ ChernRetrieval.m
@@ -0,0 +1,24 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: ChernRetrieval *)
+(* :Context: ChernRetrieval` *)
+(* :Author: c8888 *)
+(* :Date: 2016-11-20 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2016 c8888 *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["ChernRetrieval`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+
+End[] (* `Private` *)
+
+Print["Hello World!"];
+
+EndPackage[]
